671207c07100a44183b29d7e9356168320879f18
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2025-04-27 03:21:46 -0400

    features

--- MagnifyShit/MagnifyShit.cpp
+++ MagnifyShit/MagnifyShit.cpp
@@ -19,7 +19,10 @@ HINSTANCE   g_hInst;                /**< Application instance handle */
 HWND        g_hMainWnd, g_hMag;     /**< Handles for main and magnifier windows */
 int         g_zoom = 2;             /**< Magnification zoom level */
 bool        g_followMouse = false;  /**< Flag to toggle mouse-follow mode */
-const int   FOOTER_H = 28;          /**< Height of the instruction footer */
+bool g_attachWindowToMouse = false;  /**< Flag to toggle attaching the window to the mouse */
+bool g_showFooter = true;
+const int   FOOTER_H = 33;          /**< Height of the instruction footer */
+void ToggleBorderlessMode(HWND hWnd);   /**< Function to toggle borderless mode */
 
 /**
  * @brief Updates the magnifier's source region and transformation matrix.
@@ -28,16 +31,21 @@ void UpdateMag()
 {
     RECT rcWin;
     GetWindowRect(g_hMainWnd, &rcWin);
-    int w = rcWin.right - rcWin.left, h = rcWin.bottom - rcWin.top - FOOTER_H;
+    int w = rcWin.right - rcWin.left;
+    int h = rcWin.bottom - rcWin.top - (g_showFooter ? FOOTER_H : 0);
     int sw = w / g_zoom, sh = h / g_zoom;
     int sx, sy;
 
-    if (g_followMouse)
+    if (g_followMouse || g_attachWindowToMouse)
     {
         POINT pt;
         GetCursorPos(&pt);
         sx = pt.x - sw / 2;
         sy = pt.y - sh / 2;
+
+        if (g_attachWindowToMouse && (GetWindowLong(g_hMainWnd, GWL_STYLE) & WS_POPUP)) {
+            SetWindowPos(g_hMainWnd, NULL, sx, sy, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+        }
     }
     else
     {
@@ -45,7 +53,6 @@ void UpdateMag()
         sy = rcWin.top + (h - sh) / 2;
     }
 
-    // Clamp to virtual-screen bounds
     int vsx = GetSystemMetrics(SM_XVIRTUALSCREEN),
         vsy = GetSystemMetrics(SM_YVIRTUALSCREEN),
         vsw = GetSystemMetrics(SM_CXVIRTUALSCREEN),
@@ -66,15 +73,27 @@ void UpdateMag()
  */
 static void DrawFooter(HDC hdc)
 {
+    if (!g_showFooter) return;
+
     RECT rc;
     GetClientRect(g_hMainWnd, &rc);
     rc.top = rc.bottom - FOOTER_H;
-    const wchar_t* text1 = L"(Scroll Up/Down) or (Ctrl+/-) to zoom ";
-    const wchar_t* text2 = L"(Click Window) or (Ctrl+W) to toggle cursor/window follow ";
-    SetTextColor(hdc, RGB(0, 0, 0));
+    const wchar_t* text1 = L" [Ctrl+B]: Borderless Mode";
+    const wchar_t* text2 = L" [Ctrl+0]: Reset Zoom  [Ctrl +/-]: Zoom In/Out ";
+    const wchar_t* text3 = L" [Ctrl+W]: Cursor/Window Magnifier ";
+    const wchar_t* text4 = L" [Borderless Mode]: Click to attach/detach the window to cursor ";
+   
+
+    SetTextColor(hdc, RGB(0, 0, 255));
     SetBkMode(hdc, TRANSPARENT);
-    DrawTextW(hdc, text1, -1, &rc, DT_RIGHT | DT_NOCLIP | DT_SINGLELINE);
-	DrawTextW(hdc, text2, -1, &rc, DT_RIGHT | DT_NOCLIP | DT_SINGLELINE | DT_BOTTOM);
+    DrawTextW(hdc, text1, -1, &rc, DT_RIGHT | DT_NOCLIP | DT_SINGLELINE | DT_TOP);
+    DrawTextW(hdc, text2, -1, &rc, DT_RIGHT | DT_NOCLIP | DT_SINGLELINE | DT_BOTTOM);
+	DrawTextW(hdc, text3, -1, &rc, DT_LEFT | DT_NOCLIP | DT_SINGLELINE | DT_TOP);
+	DrawTextW(hdc, text4, -1, &rc, DT_LEFT | DT_NOCLIP | DT_SINGLELINE | DT_BOTTOM);
+	
+	SetTextColor(hdc, RGB(0, 0, 0));
+	SetBkMode(hdc, OPAQUE);
+
 }
 
 /**
@@ -88,11 +107,12 @@ static void DrawFooter(HDC hdc)
  */
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+    static RECT lastPosition = { 0, 0, 0, 0 };
+
     switch (msg)
     {
     case WM_CREATE:
-        //SetTimer(hWnd, 1, 16, NULL);               /**< Set timer for ~60 Hz refresh */
-        SetTimer(hWnd, 1, 8, NULL);                  /**< ~125 Hz refresh */
+        SetTimer(hWnd, 1, 8, NULL);  // ~125 Hz refresh
         SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
         break;
 
@@ -100,24 +120,42 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     {
         RECT rc;
         GetClientRect(hWnd, &rc);
-        SetWindowPos(g_hMag, NULL, 0, 0, rc.right, rc.bottom - FOOTER_H, SWP_NOZORDER);
+        int magHeight = rc.bottom;
+        if (g_showFooter)
+            magHeight -= FOOTER_H;
+
+        SetWindowPos(g_hMag, NULL, 0, 0, rc.right, magHeight, SWP_NOZORDER);
     }
     UpdateMag();
-    //InvalidateRect(hWnd, NULL, TRUE);
     break;
 
     case WM_TIMER:
         UpdateMag();
         break;
 
-    case WM_LBUTTONDOWN:                        /**< Click window toggles follow */
-        g_followMouse = !g_followMouse;
+    case WM_LBUTTONDOWN:                                                    /** Left Mouse click for borderless mode */
+        if (GetWindowLong(g_hMainWnd, GWL_STYLE) & WS_POPUP) {
+            if (g_attachWindowToMouse) {
+                g_attachWindowToMouse = false;
+            }
+            else {
+                g_attachWindowToMouse = true;
+
+                POINT pt;
+                GetCursorPos(&pt);
+
+                lastPosition.left = pt.x - (lastPosition.right - lastPosition.left) / 2;
+                lastPosition.top = pt.y - (lastPosition.bottom - lastPosition.top) / 2;
+
+                SetWindowPos(g_hMainWnd, NULL, lastPosition.left, lastPosition.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            }
+        }
         UpdateMag();
         break;
 
-    case WM_MOUSEWHEEL:
+    case WM_MOUSEWHEEL:                                                     /** ScrollWheel (Zoom In/Zoom Out) */
     {
-        int d = GET_WHEEL_DELTA_WPARAM(wParam);  /**< Vertical wheel */
+        int d = GET_WHEEL_DELTA_WPARAM(wParam);
         if (d > 0) g_zoom++; else if (g_zoom > 1) g_zoom--;
         UpdateMag();
     }
@@ -125,22 +163,39 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
     case WM_MOUSEHWHEEL:
     {
-        int d = GET_WHEEL_DELTA_WPARAM(wParam);  /**< Horizontal wheel */
+        int d = GET_WHEEL_DELTA_WPARAM(wParam);
         if (d > 0) g_zoom++; else if (g_zoom > 1) g_zoom--;
         UpdateMag();
     }
     break;
 
+	//@ Handle Ctrl B, Ctrl W, Ctrl +, Ctrl -, Ctrl 0
     case WM_KEYDOWN:
-        if (GetKeyState(VK_CONTROL) & 0x8000)         /**< Ctrl */
+        if (GetKeyState(VK_CONTROL) & 0x8000)
         {
-            if (wParam == VK_ADD || wParam == 0xBB)        /**< "+" */
-                g_zoom++;
-            else if ((wParam == VK_SUBTRACT || wParam == 0xBD) && g_zoom > 1)  /**< "-" */
-                g_zoom--;
-			else if (wParam == 'w' || wParam == 'W')    /**< "W" */
+            if (wParam == 'B' || wParam == 'b') {                           /**< Ctrl + B (toggle borderless mode) */
+                g_attachWindowToMouse = false;
+                g_followMouse = false;
+                ToggleBorderlessMode(g_hMainWnd);
+                UpdateMag();
+            }
+            else if (wParam == 'W' || wParam == 'w') {                      /**< Ctrl + W (toggle follow mode) */
                 g_followMouse = !g_followMouse;
                 UpdateMag();
+            }
+            else if (wParam == VK_ADD || wParam == VK_OEM_PLUS) {           /**< Ctrl + + (Zoom in) */
+                g_zoom++;
+                UpdateMag();
+            }
+            else if (wParam == VK_SUBTRACT || wParam == VK_OEM_MINUS) {     /**< Ctrl + - (Zoom out) */
+                if (g_zoom > 1)
+                    g_zoom--;
+                UpdateMag();
+            }
+            else if (wParam == '0' || wParam == VK_NUMPAD0) {               /**< Ctrl + 0 or Ctrl + NumPad 0 (Reset zoom to 1) */
+                g_zoom = 1;
+                UpdateMag();
+            }
         }
         break;
 
@@ -166,6 +221,35 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 }
 
 /**
+ * @brief Toggles the borderless mode of the main window.
+ *
+ * @param hWnd Handle to the main window.
+ */
+void ToggleBorderlessMode(HWND hWnd) {
+    LONG style = GetWindowLong(hWnd, GWL_STYLE);
+    if (style & WS_OVERLAPPEDWINDOW) {
+        SetWindowLong(hWnd, GWL_STYLE, style & ~WS_OVERLAPPEDWINDOW | WS_POPUP);
+        g_showFooter = false;
+    }
+    else {
+        SetWindowLong(hWnd, GWL_STYLE, (style & ~WS_POPUP) | WS_OVERLAPPEDWINDOW);
+        g_showFooter = true;
+    }
+
+    SetWindowPos(hWnd, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+    SetFocus(hWnd);
+
+    RECT rc;
+    GetClientRect(hWnd, &rc);
+    int magHeight = rc.bottom;
+    if (g_showFooter)
+        magHeight -= FOOTER_H;
+    SetWindowPos(g_hMag, NULL, 0, 0, rc.right, magHeight, SWP_NOZORDER);
+
+    InvalidateRect(hWnd, NULL, TRUE);
+}
+
+/**
  * @brief Entry point for the application.
  *
  * @param hInst Handle to the application instance.
@@ -188,10 +272,9 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow)
         WS_EX_LAYERED,
         wc.lpszClassName, L"Magnify Shit",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 1000, 700,
+        CW_USEDEFAULT, CW_USEDEFAULT, 925, 333,
         NULL, NULL, hInst, NULL);
 
-    // Set the window icon to the defined application icon
     HICON hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_MAGNIFYSHIT));
 
 
@@ -209,7 +292,11 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int nCmdShow)
 
     RECT rc;
     GetClientRect(g_hMainWnd, &rc);
-    SetWindowPos(g_hMag, NULL, 0, 0, rc.right, rc.bottom - FOOTER_H, SWP_NOZORDER);
+    int magHeight = rc.bottom;
+    if (g_showFooter)
+        magHeight -= FOOTER_H;
+
+    SetWindowPos(g_hMag, NULL, 0, 0, rc.right, magHeight, SWP_NOZORDER);
 
     MagInitialize();
 
